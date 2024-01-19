d03db76aaa613e40226bfa05d408eacf57b4910b
Yeji-Jang1210 <jyg8727@gmail.com>
2024-01-19 12:30:22 +0900

    프로젝트 초기 설정 커밋

---
+++ heyGongC_Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  heyGongC_Camera
+//
+//  Created by 장예지 on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
